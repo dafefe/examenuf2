8acb1be62149395121f23c27a4d3cf12f4faca40
alumne <alumne@a302-31>
2018-05-17 11:47:30 +0200

    Versió 1 del projecte examenuf2

---
+++ test/git/CompteTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alumne
+ */
+public class CompteTest {
+    
+    public CompteTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
